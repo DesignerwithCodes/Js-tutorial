22197d53e1b804e59bada1cb041e74b00c98ada5
DesignerwithCodes <mayurpawar130904@gmail.com>
2024-10-30 11:38:20 +0000

    commit

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,12 @@
+// alert(3+3) we are using node js not browser
+
+console.log([3+3],"Mayuresh");
+// code readability is must 
+
+let name = "Mayuresh" 
+let age =18
+let is_loggedin = false
+
+// Primitive DT : string, int, bigint, boolean, null =>Standalone Empty value , undefined =>abhi value assigned nahi hai , Symbol => unique 
+// Object
+console.log(typeof "Mayuresh")
---
+++ .devcontainer/basics/variableandconstant.js
@@ -0,0 +1,14 @@
+const account_Id = 144553 
+// const that cant be change it will be default 
+let account_email = "Mayureshpawar339@gmail.com"
+var account_password ="12345"
+account_city ="Mumbai"
+
+account_email="mayur1234@gmail.com"
+account_password ="65A425"
+account_city ="Pune"
+let account_state ;
+console.log(account_Id);
+console.table([account_Id,account_email,account_password,account_city,account_state])
+
+// Prefer not to use var bacause of issue in block scope and functional scope[{}]
