dee9469bd63e22f3987fdbbde7676dfbf4a9cb27
DesignerwithCodes <mayurpawar130904@gmail.com>
2024-10-31 08:07:44 +0000

    commit

---
+++ .devcontainer/basics/number&maths.js
@@ -0,0 +1,23 @@
+const score = 300
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const another_num = 235.236
+console.log(another_num.toPrecision(3));
+
+const payment = 100000
+console.log(payment.toLocaleString('en-In'));
+
+
+// maths session 
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.96));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(3.9));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
